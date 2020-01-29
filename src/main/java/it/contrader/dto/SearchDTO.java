d6e68c08de2c3dccff4e065b285911fa3bed71af
s-dipietro <s.dipietro1988@gmail.com>
2020-01-30 00:40:04 +0100

    Search dao, dto, converter, model, service

---
+++ src/main/java/it/contrader/dto/SearchDTO.java
@@ -0,0 +1,85 @@
+package it.contrader.dto;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class SearchDTO {
+	
+	private long search_id;
+	private int index, user_id;
+	private Date search_date; 
+	private BigDecimal value;
+	private short player;
+	
+	public SearchDTO() {}
+
+	public SearchDTO(Date search_date, BigDecimal value, int index, int user_id, short player) {
+		this.search_date = search_date;
+		this.value = value;
+		this.index = index;
+		this.user_id = user_id;
+		this.player = player;
+	}
+	
+	public SearchDTO(long search_id, Date search_date, BigDecimal value, int index, int user_id, short player) {
+		this.search_id = search_id;
+		this.search_date = search_date;
+		this.value = value;
+		this.index = index;
+		this.user_id = user_id;
+		this.player = player;
+	}
+	
+	public long getID() {
+		return search_id;
+	}
+	
+	public void setID(long search_id) {
+		this.search_id = search_id;
+	}
+	
+	public Date getDate() {
+		return search_date;
+	}
+	
+	public void setDate(Date search_date) {
+		this.search_date = search_date;
+	}
+	
+	public BigDecimal getValue() {
+		return value;
+	}
+	
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	
+	public int getUser() {
+		return user_id;
+	}
+	
+	public void setUser(int user_id) {
+		this.user_id = user_id;
+	}
+	
+	public short getPlayer() {
+		return player;
+	}
+	
+	public void setPlayer(short player) {
+		this.player = player;
+	}
+	
+	@Override
+	public String toString() {
+		return  search_id + "\t"  + search_date +"\t\t" +   value + "\t\t" + index + "\t\t" + user_id + "\t\t" + player + "\t\t";
+	}
+}
