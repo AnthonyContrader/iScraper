23bc2eeccc6e5b5f20359b0505855826e07b953b
StefanoFontana <fontanastefano24@gmail.com>
2020-01-31 21:26:43 +0100

    21.25

--- db/sampledb.sql
+++ db/sampledb.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: sampledb
+-- Host: localhost    Database: sampledb
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,53 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `injuries`
+-- Table structure for table `tb_injuries`
 --
 
-DROP TABLE IF EXISTS `injuries`;
+DROP TABLE IF EXISTS `tb_injuries`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `injuries` (
-  `ID` int DEFAULT NULL,
+CREATE TABLE `tb_injuries` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `severity` tinyint(4) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `duration` tinyint DEFAULT NULL,
-  `severity` smallint DEFAULT NULL,
-  `season` smallint DEFAULT NULL
+  `date` date DEFAULT NULL,
+  `duration` tinyint(4) DEFAULT NULL,
+  `player_id` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `player_id` (`player_id`),
+  CONSTRAINT `tb_injuries_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `tb_players` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `injuries`
+-- Dumping data for table `tb_injuries`
 --
 
-LOCK TABLES `injuries` WRITE;
-/*!40000 ALTER TABLE `injuries` DISABLE KEYS */;
-/*!40000 ALTER TABLE `injuries` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `injury`
---
-
-DROP TABLE IF EXISTS `injury`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `injury` (
-  `ID` int DEFAULT NULL,
-  `description` varchar(100) DEFAULT NULL,
-  `duration` tinyint DEFAULT NULL,
-  `severity` smallint DEFAULT NULL,
-  `season` smallint DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `injury`
---
-
-LOCK TABLES `injury` WRITE;
-/*!40000 ALTER TABLE `injury` DISABLE KEYS */;
-/*!40000 ALTER TABLE `injury` ENABLE KEYS */;
+LOCK TABLES `tb_injuries` WRITE;
+/*!40000 ALTER TABLE `tb_injuries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_injuries` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -73,14 +52,15 @@ DROP TABLE IF EXISTS `tb_players`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_players` (
-  `player_id` smallint DEFAULT NULL,
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `player_name` varchar(25) DEFAULT NULL,
   `player_surname` varchar(25) DEFAULT NULL,
-  `age` int DEFAULT NULL,
-  `actualMarketValue` int DEFAULT NULL,
-  `previousMarketValue` int DEFAULT NULL,
-  `position` varchar(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `age` int(11) DEFAULT NULL,
+  `actual_value` int(11) DEFAULT NULL,
+  `previous_value` int(11) DEFAULT NULL,
+  `position` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,7 +69,7 @@ CREATE TABLE `tb_players` (
 
 LOCK TABLES `tb_players` WRITE;
 /*!40000 ALTER TABLE `tb_players` DISABLE KEYS */;
-INSERT INTO `tb_players` VALUES (1,'wew','wrrrw',233,2323,322,'dfs'),(1,'sfssf','wwr',121,2322,3535,'dgg'),(1,'wrwr','rwrw',32,22322,32,'fsf'),(2,'fasfas','asfas',32,22432,23,'fs');
+INSERT INTO `tb_players` VALUES (1,'Cristiano','Ronaldo',34,100,120,'forward'),(2,'Lionel','Messi',32,200,210,'forward');
 /*!40000 ALTER TABLE `tb_players` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,13 +81,17 @@ DROP TABLE IF EXISTS `tb_searches`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_searches` (
-  `id` bigint NOT NULL,
+  `id` bigint(20) NOT NULL,
   `search_date` date NOT NULL,
-  `player_value` bigint NOT NULL,
-  `player_index` int NOT NULL,
-  `user` int NOT NULL,
-  `playerId` smallint NOT NULL,
-  PRIMARY KEY (`id`)
+  `player_value` bigint(20) NOT NULL,
+  `player_index` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `player_id` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `player_id` (`player_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `tb_searches_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `tb_players` (`id`),
+  CONSTRAINT `tb_searches_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -128,10 +112,10 @@ DROP TABLE IF EXISTS `tb_stats`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tb_stats` (
-  `player_id` int NOT NULL DEFAULT '0',
-  `season` varchar(9) NOT NULL DEFAULT '0',
+  `player_id` smallint(6) NOT NULL DEFAULT '0',
+  `season` int(11) NOT NULL DEFAULT '0',
   `team` varchar(32) NOT NULL DEFAULT '0',
-  `caps` int NOT NULL DEFAULT '0',
+  `caps` int(3) NOT NULL DEFAULT '0',
   `contributions` decimal(5,2) NOT NULL DEFAULT '0.00',
   `shotsper` decimal(5,2) NOT NULL DEFAULT '0.00',
   `keypass` decimal(5,2) NOT NULL DEFAULT '0.00',
@@ -142,7 +126,9 @@ CREATE TABLE `tb_stats` (
   `tackles` decimal(5,2) NOT NULL DEFAULT '0.00',
   `tacklesper` decimal(5,2) NOT NULL DEFAULT '0.00',
   `breaks` decimal(5,2) NOT NULL DEFAULT '0.00',
-  PRIMARY KEY (`player_id`,`season`)
+  PRIMARY KEY (`player_id`,`season`),
+  CONSTRAINT `tb_stats_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `tb_players` (`id`),
+  CONSTRAINT `tb_stats_ibfk_2` FOREIGN KEY (`player_id`) REFERENCES `tb_players` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -156,29 +142,29 @@ LOCK TABLES `tb_stats` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user`
+-- Table structure for table `tb_users`
 --
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `tb_users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `username` varchar(16) NOT NULL,
+CREATE TABLE `tb_users` (
+  `username` varchar(16) DEFAULT NULL,
   `usertype` varchar(255) DEFAULT NULL,
-  `password` varchar(32) NOT NULL,
-  `id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(32) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `tb_users`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('admin','ADMIN','admin',1),('user','USER','user',2);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `tb_users` WRITE;
+/*!40000 ALTER TABLE `tb_users` DISABLE KEYS */;
+INSERT INTO `tb_users` VALUES ('user','USER','user',1),('admin','ADMIN','admin',2);
+/*!40000 ALTER TABLE `tb_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -190,4 +176,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-31 14:15:40
+-- Dump completed on 2020-01-31 21:26:25
