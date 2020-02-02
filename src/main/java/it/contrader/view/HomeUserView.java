9790dad1efa7b837c78b5f514b343ff3f87be021
s-dipietro <s.dipietro1988@gmail.com>
2020-02-02 16:32:48 +0100

    Aggiunto ricerche a HomeUserView

--- src/main/java/it/contrader/view/HomeUserView.java
+++ src/main/java/it/contrader/view/HomeUserView.java
@@ -21,7 +21,7 @@ public class HomeUserView extends AbstractView{
 	public void showOptions() {
 		System.out.println("-------------MENU------------\n");
 		System.out.println("Seleziona su che tabella lavorare");
-		System.out.println("\n[G]iocatori [I]nfortuni [T]eams [S]tatistiche [E]sci");
+		System.out.println("\n[G]iocatori [I]nfortuni [T]eams [S]tatistiche [R]icerche [E]sci");
 		choice = this.getInput();
 
 	}
@@ -48,6 +48,10 @@ public class HomeUserView extends AbstractView{
 			this.request.put("mode", "STATSBOXLIST");
 			MainDispatcher.getInstance().callAction("Statsbox", "doControl", request);
 			break;
+		case "r":
+        	this.request.put("mode", "SEARCHLIST");
+        	MainDispatcher.getInstance().callAction("Search", "doControl", request);
+        	break;
 			
 		case "e":
 			MainDispatcher.getInstance().callAction("Login", "doControl", null);
