15d170e15bfc883633896d010dd196dbf7c56369
StefanoFontana <fontanastefano24@gmail.com>
2020-02-12 10:57:26 +0100

    fixed bean Crud problem

--- src/main/java/it/contrader/service/PlayerService.java
+++ src/main/java/it/contrader/service/PlayerService.java
@@ -3,8 +3,6 @@ package it.contrader.service;
 
 import java.util.List;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,36 +14,16 @@ import it.contrader.dto.PlayerDTO;
 import it.contrader.model.Player;
 import it.contrader.model.Team;
 
-import lombok.AllArgsConstructor;
-
-
-@AllArgsConstructor
-
-
-//@Entity
 
 @Service
 public class PlayerService extends AbstractService<Player, PlayerDTO>{
 
-	
-	private PlayerConverter playerConverter;
-	private final PlayerRepository playerRepository;
-	
 	@Autowired
-	public  PlayerService(PlayerRepository playerRepository) {
-		
-		this.playerRepository=playerRepository;
-		// TODO Auto-generated constructor stub
-	}
+	private PlayerConverter playerConverter;
+	@Autowired
+	private PlayerRepository playerRepository;
 	
 	public List<PlayerDTO> findByTeam(Team team) {
 		return playerConverter.toDTOList(playerRepository.findByTeam(team));
 	}
-	
-	
-	
-
-	
-	
-	
 }
