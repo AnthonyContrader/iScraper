2bf25f8c855d71dbed1dfb5365b124ea8d77911e
StefanoFontana <fontanastefano24@gmail.com>
2020-02-17 14:21:50 +0100

    statsbox inizializzato

---
+++ src/dto/statsboxdto.ts
@@ -0,0 +1,33 @@
+import {Player} from './playerdto'
+
+export class StatsboxDTO {
+
+   id: number;
+
+   player: Player;
+
+   season: number;
+
+   caps: number;
+    
+   contributions: number;
+        
+   shotsper: number;
+    
+   keypass: number; 
+    
+   passprec: number;
+    
+   dribbling: number;
+    
+   foulssub: number; 
+    
+   foulscomm: number;
+        
+   tackles: number; 
+    
+   tacklesper: number; 
+        
+   breaks: number;
+}
+
