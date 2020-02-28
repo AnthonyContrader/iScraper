4417ca6d74672b8e26ef724c24756a5a0299c350
majorsky <freddy3ro@yahoo.com>
2020-02-28 09:53:25 +0100

    .

--- src/main/java/it/contrader/SpringWebApplication.java
+++ src/main/java/it/contrader/SpringWebApplication.java
@@ -20,11 +20,7 @@ public class SpringWebApplication {
 
 	public static void main(String[] args) {
 		
-//		Team team=new Team();
-//		team.setName("Milan");
-//		PlayerService service=new PlayerService();
-//		PlayerDTO playerDTO= new PlayerDTO( 3,"adas","ada",2,2,2,"www",team);
-//		service.update(playerDTO);
+
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
 }
